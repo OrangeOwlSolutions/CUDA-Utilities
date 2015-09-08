8126c7cbaa24690e5e513fa59de7165a69950090
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-09-08 10:17:22 +0200

    Update Matlab_like.cuh

--- Matlab_like.cuh
+++ Matlab_like.cuh
@@ -1,6 +1,17 @@
 #ifndef MATLAB_LIKE_CUH
 #define MATLAB_LIKE_CUH
 
+/************/
+/* LINSPACE */
+/************/
 template <class T> T * linspace(const T, const T, const unsigned int);
+
+/************/
+/* MESHGRID */
+/************/
+#include <thrust/pair.h>
+
+template <class T>
+thrust::pair<T *,T *> meshgrid(const T *, const unsigned int, const T *, const unsigned int);
 
 #endif
