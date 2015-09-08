9172f0a75184bf01386df48de8bb35c149865ea7
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-09-08 16:42:23 +0200

    Update Utilities.cuh

--- Utilities.cuh
+++ Utilities.cuh
@@ -3,12 +3,143 @@
 
 #include <cusolverDn.h>
 #include <cublas_v2.h>
-#include <cufft.h>
 
 extern "C" int iDivUp(int, int);
 extern "C" void gpuErrchk(cudaError_t);
 extern "C" void cusolveSafeCall(cusolverStatus_t);
 extern "C" void cublasSafeCall(cublasStatus_t);
-extern "C" void cufftSafeCall(cufftResult);
+
+template <class T>
+void reverseArray(const T * __restrict__, T * __restrict__, const int);
+
+/**************************/
+/* TEMPLATE SHARED MEMORY */
+/**************************/
+// --- Credit to the simpleTemplates CUDA sample
+template <typename T>
+struct SharedMemory
+{
+    // Ensure that we won't compile any un-specialized types
+    __device__ T *getPointer()
+    {
+        extern __device__ void error(void);
+        error();
+        return NULL;
+    }
+};
+
+// Following are the specializations for the following types.
+// int, uint, char, uchar, short, ushort, long, ulong, bool, float, and double
+// One could also specialize it for user-defined types.
+
+template <>
+struct SharedMemory <int>
+{
+    __device__ int *getPointer()
+    {
+        extern __shared__ int s_int[];
+        return s_int;
+    }
+};
+
+template <>
+struct SharedMemory <unsigned int>
+{
+    __device__ unsigned int *getPointer()
+    {
+        extern __shared__ unsigned int s_uint[];
+        return s_uint;
+    }
+};
+
+template <>
+struct SharedMemory <char>
+{
+    __device__ char *getPointer()
+    {
+        extern __shared__ char s_char[];
+        return s_char;
+    }
+};
+
+template <>
+struct SharedMemory <unsigned char>
+{
+    __device__ unsigned char *getPointer()
+    {
+        extern __shared__ unsigned char s_uchar[];
+        return s_uchar;
+    }
+};
+
+template <>
+struct SharedMemory <short>
+{
+    __device__ short *getPointer()
+    {
+        extern __shared__ short s_short[];
+        return s_short;
+    }
+};
+
+template <>
+struct SharedMemory <unsigned short>
+{
+    __device__ unsigned short *getPointer()
+    {
+        extern __shared__ unsigned short s_ushort[];
+        return s_ushort;
+    }
+};
+
+template <>
+struct SharedMemory <long>
+{
+    __device__ long *getPointer()
+    {
+        extern __shared__ long s_long[];
+        return s_long;
+    }
+};
+
+template <>
+struct SharedMemory <unsigned long>
+{
+    __device__ unsigned long *getPointer()
+    {
+        extern __shared__ unsigned long s_ulong[];
+        return s_ulong;
+    }
+};
+
+template <>
+struct SharedMemory <bool>
+{
+    __device__ bool *getPointer()
+    {
+        extern __shared__ bool s_bool[];
+        return s_bool;
+    }
+};
+
+template <>
+struct SharedMemory <float>
+{
+    __device__ float *getPointer()
+    {
+        extern __shared__ float s_float[];
+        return s_float;
+    }
+};
+
+template <>
+struct SharedMemory <double>
+{
+    __device__ double *getPointer()
+    {
+        extern __shared__ double s_double[];
+        return s_double;
+    }
+};
 
 #endif
