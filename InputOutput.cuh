bf8104b0b7495f962524f0028e5bd85b4c7155dc
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-10-05 23:17:31 +0200

    Update InputOutput.cuh

--- InputOutput.cuh
+++ InputOutput.cuh
@@ -5,7 +5,12 @@ template <class T>
 void saveGPUrealtxt(const T *, const char *, const int);
 
 template <class T>
+void saveCPUrealtxt(const T *, const char *, const int);
+
+template <class T>
 void saveGPUcomplextxt(const T *, const char *, const int);
+
+void saveGPUcomplextxt(const double2 *, const char *, const int);
 
 template <class T>
 T * loadGPUrealtxt(const char *, T * __restrict__, const int);
