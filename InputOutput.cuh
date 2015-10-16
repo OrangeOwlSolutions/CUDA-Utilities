8e065d0cfdf327fb7105721cd72b6d658cfc3dd0
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-10-16 10:56:11 +0200

    Create InputOutput.cuh

---
+++ InputOutput.cuh
@@ -0,0 +1,21 @@
+#ifndef INPUTOUTPUT_H
+#define INPUTOUTPUT_H
+
+template <class T>
+void saveGPUrealtxt(const T *, const char *, const int);
+
+template <class T>
+void saveCPUrealtxt(const T *, const char *, const int);
+
+template <class T>
+void saveGPUcomplextxt(const T *, const char *, const int);
+
+void saveGPUcomplextxt(const double2 *, const char *, const int);
+
+template <class T>
+T * loadCPUrealtxt(const char *, T * __restrict__, const int);
+
+template <class T>
+T * loadGPUrealtxt(const char *, T * __restrict__, const int);
+
+#endif
