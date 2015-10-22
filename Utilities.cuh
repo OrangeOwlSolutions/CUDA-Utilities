7f2154893abf66ba6e2410cd8150822d7438b19b
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-10-22 17:22:07 +0200

    Update Utilities.cuh

--- Utilities.cuh
+++ Utilities.cuh
@@ -1,7 +1,7 @@
 #ifndef UTILITIES_CUH
 #define UTILITIES_CUH
 
-//#include <cusolverDn.h>
+#include <cusolverDn.h>
 #include <cublas_v2.h>
 #include <cusparse_v2.h>
 #include <cufft.h>
@@ -10,7 +10,7 @@
 
 extern "C" int iDivUp(int, int);
 extern "C" void gpuErrchk(cudaError_t);
-//extern "C" void cusolveSafeCall(cusolverStatus_t);
+extern "C" void cusolveSafeCall(cusolverStatus_t);
 extern "C" void cublasSafeCall(cublasStatus_t);
 extern "C" void cufftSafeCall(cufftResult err);
 extern "C" void cusparseSafeCall(cusparseStatus_t err);
