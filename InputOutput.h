2c622ea57933940272035d90ba3613a05bf09988
Orange Owl <orangeowlsolutions@gmail.com>
2016-07-15 07:49:55 +0200

    Update InputOutput.h

--- InputOutput.h
+++ InputOutput.h
@@ -5,6 +5,10 @@ template <class T>
 void saveCPUrealtxt(const T *, const char *, const int);
 
 template <class T>
-T * loadCPUrealtxt(const char *, T * __restrict__, const int);
+void saveCPUintegertxt(const T *, const char *, const int);
+
+template <class T>
+void loadCPUrealtxt(T * __restrict, const char *, const int);
+//T * loadCPUrealtxt(T * __restrict, const char *, const int);
 
 #endif
