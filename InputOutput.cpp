7e78a5b31727c2750ac23871102109569d9fda93
Orange Owl <orangeowlsolutions@gmail.com>
2016-07-15 07:49:21 +0200

    Update InputOutput.cpp

--- InputOutput.cpp
+++ InputOutput.cpp
@@ -11,36 +11,56 @@
 /***********************************************/
 template <class T>
 void saveCPUrealtxt(const T * h_in, const char *filename, const int M) {
-	
+
 	std::ofstream outfile;
 	outfile.open(filename);
-	for(int i = 0; i < M; i++) outfile << std::setprecision(prec_save) << h_in[i] << "\n"; 
+	for (int i = 0; i < M; i++) outfile << std::setprecision(prec_save) << h_in[i] << "\n";
 	outfile.close();
 
 }
 
-template void saveCPUrealtxt<float> (const float  *, const char *, const int);
+template void saveCPUrealtxt<float>(const float  *, const char *, const int);
 template void saveCPUrealtxt<double>(const double *, const char *, const int);
+
+/**************************************************/
+/* SAVE INDIVIDUAL INTEGER CPU MATRIX TO txt FILE */
+/**************************************************/
+template <class T>
+void saveCPUintegertxt(const T * h_in, const char *filename, const int M) {
+
+	std::ofstream outfile;
+	outfile.open(filename);
+	for (int i = 0; i < M; i++) outfile << h_in[i] << "\n";
+	outfile.close();
+
+}
+
+template void saveCPUintegertxt<int>(const int  *, const char *, const int);
 
 /****************************************************/
 /* LOAD INDIVIDUAL REAL MATRIX FROM txt FILE TO CPU */
 /****************************************************/
 // --- Load individual real matrix from txt file
 template <class T>
-T * loadCPUrealtxt(const char *filename, T * __restrict__ h_out, const int M) {
-		
-	h_out	= (T *)malloc(M * sizeof(T));
+void loadCPUrealtxt(T * __restrict h_out, const char *filename, const int M) {
+//T * loadCPUrealtxt(T * __restrict h_out, const char *filename, const int M) {
+
+	//h_out = (T *)malloc(M * sizeof(T));
 
 	std::ifstream infile;
 	infile.open(filename);
-	for(int i = 0; i < M; i++) infile >> h_out[i]; 
+	for (int i = 0; i < M; i++) infile >> h_out[i]; 
+
 	infile.close();
 
-	return h_out;
+	//return h_out;
 
 }
 
-template float  * loadCPUrealtxt<float> (const char *, float  * __restrict__, const int);
-template double * loadCPUrealtxt<double>(const char *, double * __restrict__, const int);
+template void loadCPUrealtxt<int>(int * __restrict, const char *, const int);
+template void loadCPUrealtxt<float>(float  * __restrict, const char *, const int);
+template void loadCPUrealtxt<double>(double * __restrict, const char *, const int);
+//template float  * loadCPUrealtxt<float>(float  * __restrict, const char *, const int);
+//template double * loadCPUrealtxt<double>(double * __restrict, const char *, const int);
 
 
