621f27be7024a399a4e11642a2b03db3e788cb5e
Orange Owl <orangeowlsolutions@gmail.com>
2018-10-31 18:07:33 +0100

    Update cublasWrappers.cu

--- cublasWrappers.cu
+++ cublasWrappers.cu
@@ -1,4 +1,4 @@
-#include "cublas_v2.h"
+#include "cublasWrappers.cuh"
 
 /**************/
 /* CUBLASTDOT */
@@ -11,6 +11,28 @@ cublasStatus_t cublasTdot(cublasHandle_t handle, int n, const double *x, int inc
 	return cublasDdot(handle, n, x, incx, y, incy, result);
 }
 
+/***************/
+/* CUBLASTNRM2 */
+/***************/
+cublasStatus_t cublasTnrm2(cublasHandle_t handle, int n, const float *x, int incx, float *result) {
+	return cublasSnrm2(handle, n, x, incx, result);
+}
+
+cublasStatus_t cublasTnrm2(cublasHandle_t handle, int n, const double *x, int incx, double *result) {
+	return cublasDnrm2(handle, n, x, incx, result);
+}
+
+/***************/
+/* CUBLASTSCAL */
+/***************/
+cublasStatus_t cublasTscal(cublasHandle_t handle, int n, const float *alpha, float *x, int incx) {
+	return cublasSscal(handle, n, alpha, x, incx);
+}
+	
+cublasStatus_t cublasTscal(cublasHandle_t handle, int n, const double *alpha, double *x, int incx) {
+	return cublasDscal(handle, n, alpha, x, incx);
+}
+	
 /***************/
 /* CUBLASTAXPY */
 /***************/
@@ -29,7 +51,7 @@ cublasStatus_t cublasTcopy(cublasHandle_t handle, int n, const float *x, int inc
 	return cublasScopy(handle, n, x, incx, y, incy);
 }
 
-cublasStatus_t cublastcopy(cublasHandle_t handle, int n, const double *x, int incx, double *y, int incy) {
+cublasStatus_t cublasTcopy(cublasHandle_t handle, int n, const double *x, int incx, double *y, int incy) {
 	return cublasDcopy(handle, n, x, incx, y, incy);
 }
 
