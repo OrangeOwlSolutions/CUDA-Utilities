70343897abbf7a5608a6739759437f44933a5fc6
Orange Owl <orangeowlsolutions@gmail.com>
2018-10-31 18:07:54 +0100

    Update cublasWrappers.cuh

--- cublasWrappers.cuh
+++ cublasWrappers.cuh
@@ -1,11 +1,25 @@
 #ifndef CUBLASWRAPPERS_CUH
 #define CUBLASWRAPPERS_CUH
 
+#include "cublas_v2.h"
+
 /**************/
 /* CUBLASTDOT */
 /**************/
 cublasStatus_t cublasTdot(cublasHandle_t, int, const float  *, int, const float  *, int, float  *);
 cublasStatus_t cublasTdot(cublasHandle_t, int, const double *, int, const double *, int, double *);
+
+/***************/
+/* CUBLASTNRM2 */
+/***************/
+cublasStatus_t cublasTnrm2(cublasHandle_t, int, const float *, int, float *);
+cublasStatus_t cublasTnrm2(cublasHandle_t, int, const double *, int, double *);
+
+/***************/
+/* CUBLASTSCAL */
+/***************/
+cublasStatus_t cublasTscal(cublasHandle_t, int, const float *, float *, int);
+cublasStatus_t cublasTscal(cublasHandle_t, int, const double *, double *, int);
 
 /***************/
 /* CUBLASTAXPY */
@@ -17,6 +31,6 @@ cublasStatus_t cublasTaxpy(cublasHandle_t, int, const double *, const double *, 
 /* CUBLASTCOPY */
 /***************/
 cublasStatus_t cublasTcopy(cublasHandle_t, int, const float *, int, float *, int);
-cublasStatus_t cublastcopy(cublasHandle_t, int, const double *, int, double *, int);
+cublasStatus_t cublasTcopy(cublasHandle_t, int, const double *, int, double *, int);
 
 #endif
